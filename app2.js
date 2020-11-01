4c9c8c890c9e59ea0efbe1a765aa77cbb28cb227
daichi_sato <daichiexperience@gmail.com>
2020-11-01 22:01:19 +0900

    [Add]
    Vueの基本的な構文を理解する。
    v-model, v-for, v-onでそれぞれ何ができるのかを確認する。

---
+++ app2.js
@@ -0,0 +1,10 @@
+const vm = new Vue({
+  el: "#app",
+  data: {
+    items: [
+      { title: "something1", id: 1 },
+      { title: "something2", id: 2 },
+      { title: "something3", id: 3 },
+    ],
+  },
+});
