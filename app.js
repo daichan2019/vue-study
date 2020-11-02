4cfe803b565e3d171ba67d5d5c5f211ca9f3b205
daichi_sato <daichiexperience@gmail.com>
2020-11-02 10:01:16 +0900

    [Add]
    Vueのtransition要素を使用し、animationを追加する方法を学ぶ

---
+++ app.js
@@ -0,0 +1,16 @@
+const vm = new Vue({
+  el: "#demo",
+  data: {
+    show: true,
+  },
+
+  el: "#demo2",
+  data: {
+    show: true,
+  },
+
+  el: "#demo3",
+  data: {
+    show: true,
+  },
+});
